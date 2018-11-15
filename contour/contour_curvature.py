7ccf325c6ac94b277c6481ea69fc70eeabd81a16
wsyxbcl <yx_chai@whu.edu.cn>
2018-11-15 15:48:03 +0800

    contour part finished

---
+++ contour/contour_curvature.py
@@ -0,0 +1,125 @@
+import os
+
+import numpy as np
+from imageio import imread
+from scipy.ndimage import convolve
+import matplotlib
+import matplotlib.pyplot as plt
+import morphsnakes as ms
+from pathlib import Path
+
+from curvature import cal_curvature
+
+
+def visual_callback_2d(background, fig=None):
+    """
+    Returns a callback than can be passed as the argument `iter_callback`
+    of `morphological_geodesic_active_contour` and
+    `morphological_chan_vese` for visualizing the evolution
+    of the levelsets. Only works for 2D images.
+    
+    Args
+        background: (M, N) array
+            Image to be plotted as the background of the visual evolution.
+        fig: matplotlib.figure.Figure
+            Figure where results will be drawn. If not given, a new figure
+            will be created.
+    
+    Returns
+        callback: Python function
+            A function that receives a levelset and updates the current plot
+            accordingly. This can be passed as the `iter_callback` argument of
+            `morphological_geodesic_active_contour` and
+            `morphological_chan_vese`.
+    
+    """
+    # Prepare the visual environment.
+    if fig is None:
+        fig = plt.figure()
+    fig.clf()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.imshow(background, cmap=plt.cm.gray)
+
+    def callback(levelset):
+        global img_contour
+        # print(levelset)
+  
+        if ax.collections:
+            del ax.collections[0]
+        img_contour = ax.contour(levelset, [0.5], colors='r').allsegs[0][0]
+        fig.canvas.draw()
+        plt.pause(0.001)
+
+    return callback
+
+def rgb2gray(img):
+    """
+    Convert a RGB image to gray scale.
+    """
+    return 0.2989 * img[..., 0] + 0.587 * img[..., 1] + 0.114 * img[..., 2]
+
+def smooth(x, kernel_size):
+    """
+    circular convolution(wrap mode) on 1d array to smooth the line
+    """
+    kernel = np.ones(kernel_size)/kernel_size
+    # x_smooth = np.convolve(x, kernel, mode='valid') # edge info lost
+    x_smooth = convolve(x, kernel, mode='wrap')
+    return x_smooth
+
+def example_tem():
+    # Load the image.
+    imgcolor = imread(str(PATH_IMG_TEM))/255.0
+    img = rgb2gray(imgcolor)
+    
+    # Initialization of the level-set.
+    init_ls = ms.circle_level_set(img.shape, radius=min(img.shape) * 3.8 / 8.0)
+    # Callback for visual plotting
+    callback = visual_callback_2d(imgcolor)
+
+    # Morphological Chan-Vese
+    ms.morphological_chan_vese(img, iterations=50,
+                               init_level_set=init_ls,
+                               smoothing=3, lambda1=1, lambda2=7.5,
+                               iter_callback=callback)
+
+
+if __name__ == '__main__':
+
+    PATH_IMG_TEM = Path('../images/tem1.tif')
+
+    # Global variable to store coordinates of contour points
+    global img_contour
+    img_contour = []
+
+    example_tem()
+    # plt.show()
+    
+    # Calculate the curvature
+    x = img_contour[:, 0]
+    y = img_contour[:, 1]
+    x_smooth = smooth(x, 100)
+    y_smooth = smooth(y, 100)
+
+    curvature = cal_curvature(x_smooth, y_smooth, interval=50)
+    curvature_sorted = sorted(curvature)
+    # curvature_min = curvature_sorted[int(0.2*len(curvature))]
+    # curvature_max = curvature_sorted[-int(0.2*len(curvature))]   
+    curvature_min = curvature_sorted[0]
+    curvature_max = curvature_sorted[-1]   
+
+    # Plot and color mapping
+    colormap = plt.get_cmap('jet')
+    color_norm = matplotlib.colors.Normalize(vmin=curvature_min, vmax=curvature_max)
+    scalar_map = matplotlib.cm.ScalarMappable(norm=color_norm, cmap=colormap)
+    scalar_map.set_array(curvature)
+
+    fig = plt.figure()
+    imgcolor = imread(str(PATH_IMG_TEM))/255.0
+    img = rgb2gray(imgcolor)
+    ax = fig.add_subplot(1, 1, 1)
+    ax.imshow(img, cmap=plt.cm.gray)
+    ax.scatter(x, y, c=scalar_map.to_rgba(curvature), marker='.', s=10)
+    # ax.plot(x, y, c=scalar_map.to_rgba(curvature))
+    plt.colorbar(scalar_map)
+    plt.show()
---
+++ contour/curvature.py
@@ -0,0 +1,90 @@
+import matplotlib.pyplot as plt
+
+def find_circle(b, c, d):
+    """
+    Use 3 points(a, b and c) to fix a circle.
+    """
+    temp = c[0]**2 + c[1]**2
+    bc = (b[0]**2 + b[1]**2 - temp) / 2
+    cd = (temp - d[0]**2 - d[1]**2) / 2
+    det = (b[0] - c[0]) * (c[1] - d[1]) - (c[0] - d[0]) * (b[1] - c[1])
+
+    if abs(det) < 1.0e-10:
+        raise ValueError('Points on same line')
+
+    # Center of circle
+    center_x = (bc*(c[1] - d[1]) - cd*(b[1] - c[1])) / det
+    center_y = ((b[0] - c[0]) * cd - (c[0] - d[0]) * bc) / det
+
+    radius = ((center_x - b[0])**2 + (center_y - b[1])**2)**.5
+
+    return center_x, center_y, radius
+
+def cal_curvature(x, y, cyc=1, interval=1, sign=1):
+    """
+    Calculate the curvature of each point of a given curve.
+    At each point, we calculate the curvature by fitting a circle to that
+    point and the two points that are #interval points away from it. 
+
+    The curvature is then defined as the reciprocal of the radius of that circle
+
+    Args
+        x, y: lists that contains coordinates of points on the curve
+        cyc: cyc=1 if the curve in in a cyclic form
+        interval: number of points between the middle point and the other two points
+        sign: whether to distinguish the sign of the curvature
+        
+    Returns
+        cv: list of curvature at each point
+    """
+    cv = []
+    for i in range(len(x)):
+        if cyc:
+            x_midx, x_lidx, x_hidx = x[i], x[i-interval], x[(i+interval)%len(x)]
+            y_midx, y_lidx, y_hidx = y[i], y[i-interval], y[(i+interval)%len(y)]
+        else:
+            if i - interval < 0:
+                x_midx, x_lidx, x_hidx = x[i], x[i], x[i+interval]
+                y_midx, y_lidx, y_hidx = y[i], y[i], y[i+interval]
+            elif i + interval > (len(x) - 1):
+                x_midx, x_lidx, x_hidx = x[i], x[i-interval], x[i]
+                y_midx, y_lidx, y_hidx = y[i], y[i-interval], y[i]
+            else:
+                x_midx, x_lidx, x_hidx = x[i], x[i-interval], x[i+interval]
+                y_midx, y_lidx, y_hidx = y[i], y[i-interval], y[i+interval]    
+
+        if sign:
+            s = (x_lidx - x_midx) * (y_hidx - y_midx) - (y_lidx - y_midx) * (x_hidx - x_midx)
+        else:
+            s = 1
+
+        try:
+            center_x, center_y, radius = find_circle([x_midx, y_midx],
+                                                 [x_lidx, y_lidx],
+                                                 [x_hidx, y_hidx])
+        except ValueError:
+            cv.append(0.0)
+        if s > 0:
+            cv.append(1.0/-radius)
+        else:
+            cv.append(1.0/radius)
+    return cv
+
+
+if __name__ == '__main__':
+    a = [1, 2]
+    b = [2, 2]
+    c = [3, 5]
+
+    center_x, center_y, radius = find_circle(a, b, c)
+    circle = plt.Circle((center_x, center_y), radius, color = 'r', fill=False)
+    # circle = plt.Circle((0, 0), 1, color = 'r', fill=False)
+
+    fig, ax = plt.subplots()
+
+    ax.add_artist(circle)
+    ax.plot(a[0], a[1], 'o')
+    ax.plot(b[0], b[1], 'o')
+    ax.plot(c[0], c[1], 'o')
+    plt.show()
+    cv = cal_curvature([1,2,3], [2,2,5])
