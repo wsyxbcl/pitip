67b66b6af16f40a938404016c6a6e800d746c392
wsyxbcl <wsyxbcl@gmail.com>
2019-06-18 00:00:16 +0800

    try denoising

--- counting/blob.py
+++ counting/blob.py
@@ -1,26 +1,35 @@
 import matplotlib.pyplot as plt
-import cv2 as cv
+import cv2
 import numpy as np
 
 
-# Try opencv simpleblobdetector first
+# Try opencv2.simpleblobdetector first
 
 # Read image
-img = cv.imread("../images/40000.jpeg", 0)
+img = cv2.imread("../images/particles1.jpg", 0)
+# img = cv2.imread("../images/particles.jpg", 0)
+# Denoising
+
+img_denoise = cv2.fastNlMeansDenoising(img, None, 30, 7, 21)
+# plt.subplot(121)
+# plt.imshow(img)
+# plt.subplot(122)
+# plt.imshow(img_denoise)
+# plt.show()
 
 # Setup SimpleBlobDetector parameters.
-params = cv.SimpleBlobDetector_Params()
+params = cv2.SimpleBlobDetector_Params()
 
 # Change thresholds
-params.minThreshold = 150
-params.maxThreshold = 255
+params.minThreshold = 10
+params.maxThreshold = 200
 
 # Set edge gradient
-params.thresholdStep = 5
+# params.thresholdStep = 5
 
 # Filter by Area.
-params.filterByArea = True
-params.minArea = 20
+# params.filterByArea = True
+# params.minArea = 10
 # params.maxArea = 100
 
 # Filter by Color
@@ -28,32 +37,34 @@ params.minArea = 20
 # params.blobColor = 255
 
 # Set up the detector with default parameters.
-detector = cv.SimpleBlobDetector_create(params)
+detector = cv2.SimpleBlobDetector_create(params)
 
 # Detect blobs.
 keypoints = detector.detect(img)
 
 # Draw detected blobs as red circles.
-# cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-im_with_keypoints = cv.drawKeypoints(img, keypoints, np.array([]), (0, 0, 255),
-                                      cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+im_with_keypoints = cv2.drawKeypoints(img_denoise, keypoints, np.array([]), (0, 0, 255),
+                                      cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 # Show keypoints
-plt.subplot(121)
+plt.subplot(131)
 plt.imshow(img, cmap = 'gray')
-plt.subplot(122)
+plt.subplot(132)
+plt.imshow(img_denoise, cmap = 'gray')
+plt.subplot(133)
 plt.imshow(im_with_keypoints)
 
 plt.show()
 
 # Try different threshold
 
-# img = cv.medianBlur(img,5)
-# ret,th1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
-# th2 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,\
-#             cv.THRESH_BINARY,11,2)
-# th3 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
-#             cv.THRESH_BINARY,11,2)
+# img = cv2.medianBlur(img,5)
+# ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+# th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+#             cv2.THRESH_BINARY,11,2)
+# th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+#             cv2.THRESH_BINARY,11,2)
 # titles = ['Original Image', 'Global Thresholding (v = 127)',
 #             'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
 # images = [img, th1, th2, th3]
