9f5b7e2f555a656ac3db45f3dcadf568ec64ecd0
wsyxbcl <wsyxbcl@gmail.com>
2019-07-13 17:18:45 +0800

    calculation of psi_6

--- particle/boo.py
+++ particle/boo.py
@@ -6,9 +6,14 @@ from distance import distance_calc
 distances = distance_calc(particles)
 
 cutoff_distance =
-distance_filters = (distances <= cutoff_distance)
-for j_idx, distance_filter in distance_filters:
+distance_filter_matrix = (distances <= cutoff_distance)
+np.fill_diagonal(distance_filter_matrix, 0)
+
+phi_6_j = []
+for j_idc, distance_filter in enumerate(distance_filter_matrix):
     k_idx = np.where(distance_filter == 1)[0]
-    n_k = k.size
-    bond_jk = particles[k] - particles[j]
-    theta_jk = np.arctan2(bond_jk[1], bond_jk[0]) #TODO could be vectorized
+    n_k = k_idx.size 
+    bond_jk = particles[k_idx] - particles[j_idc] # n_k * 2 matrix
+    theta_jk = np.arctan2(bond_jk[:, 1], bond_jk[:, 0])
+    phi_6_j.append(np.average(np.exp(6j*theta_jk)))
+psi_6 = np.average(phi_6_j)
