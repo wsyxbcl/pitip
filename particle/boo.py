260751f123cf9759ca08696813f6c6a85ab32ce3
wsyxbcl <wsyxbcl@gmail.com>
2019-07-16 02:42:23 +0800

    reconstruct & preparing a test case

--- particle/boo.py
+++ particle/boo.py
@@ -1,19 +1,37 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 from distance import distance_calc
 
-distances = distance_calc(particles)
+def phi_6(points, cutoff_distance):
+    """
+    Input
+        points: n*2 numpy array that contains n coordinates of points
+        cutoff_distance: two points are defined as nearest neighbors if
+                         the separation is within the cutoff_distance.
+                         (for now, unit: pixel)
+    Return
+        phi_6_j: np array, n_phi_6 of every point j
 
-cutoff_distance =
-distance_filter_matrix = (distances <= cutoff_distance)
-np.fill_diagonal(distance_filter_matrix, 0)
+    """
+    distances = distance_calc(points)
+    distance_filter_matrix = (distances <= cutoff_distance)
+    np.fill_diagonal(distance_filter_matrix, 0)
 
-phi_6_j = []
-for j_idc, distance_filter in enumerate(distance_filter_matrix):
-    k_idx = np.where(distance_filter == 1)[0]
-    n_k = k_idx.size 
-    bond_jk = particles[k_idx] - particles[j_idc] # n_k * 2 matrix
-    theta_jk = np.arctan2(bond_jk[:, 1], bond_jk[:, 0])
-    phi_6_j.append(np.average(np.exp(6j*theta_jk)))
-psi_6 = np.average(phi_6_j)
+    phi_6_j = []
+    for j_idc, distance_filter in enumerate(distance_filter_matrix):
+        k_idx = np.where(distance_filter == 1)[0]
+        n_k = k_idx.size 
+        bond_jk = points[k_idx] - points[j_idc] # n_k * 2 matrix
+        theta_jk = np.arctan2(bond_jk[:, 1], bond_jk[:, 0])
+        phi_6_j.append(np.average(np.exp(6j*theta_jk)))
+    # psi_6 = np.average(phi_6_j)
+    return phi_6_j
+
+if __name__ == '__main__':
+    # Test case
+    particles = np.random.rand(30, 2)
+    # plt.scatter(particles[:, 0], particles[:, 1])
+    # plt.show()
+    phi_6_j = phi_6(particles, cutoff_distance=0.1)
