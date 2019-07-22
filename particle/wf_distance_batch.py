9b50170d059558c3a845af8250c324bc1281b4bd
wsyxbcl <wsyxbcl@gmail.com>
2019-07-22 22:59:25 +0800

    add blob detection & make a function

--- particle/wf_distance_batch.py
+++ particle/wf_distance_batch.py
@@ -1,40 +1,78 @@
 import cv2
 import numpy as np
+import sys
+sys.path.insert(0, '../contour')
 
-# Create a black image, a window
-img = cv2.imread("../images/particles.jpg", 0)
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-# cv2.setWindowProperty('image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+from blob import particle_blob_detection
 
-# create trackbars for denoising
-cv2.createTrackbar('NlMean', 'image', 0, 1, lambda x: None) # fastNlMeansDenoising
-cv2.createTrackbar('h', 'image', 10, 100, lambda x: None)
-cv2.createTrackbar('tws', 'image', 3, 49, lambda x: None) # 0.5(templateWindowSize-1)
-cv2.createTrackbar('sws', 'image', 10, 49, lambda x: None) # 0.5(searchWindowSize-1)
+def find_best_blob(img)
+    """
+    Find proper parameter for denoising and blob detection.
+    """
+    # Create a black image, a window
+    # img = cv2.imread("../images/particles.jpg", 0)
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    # cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE)
+    cv2.resizeWindow('image', 500, 500)
+    # cv2.setWindowProperty('image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-cv2.createTrackbar('Median', 'image', 0, 1, lambda x: None) # medianBlur
-cv2.createTrackbar('k', 'image', 2, 49, lambda x: None) # 0.5(ksize-1)
+    # create trackbars for denoising
+    cv2.createTrackbar('NlMean', 'image', 0, 1, lambda x: None) # fastNlMeansDenoising
+    cv2.createTrackbar('h', 'image', 10, 100, lambda x: None)
+    cv2.createTrackbar('tws', 'image', 3, 49, lambda x: None) # 0.5(templateWindowSize-1)
+    cv2.createTrackbar('sws', 'image', 10, 49, lambda x: None) # 0.5(searchWindowSize-1)
 
-cv2.imshow('image', img)
+    cv2.createTrackbar('Median', 'image', 0, 1, lambda x: None) # medianBlur
+    cv2.createTrackbar('k', 'image', 2, 49, lambda x: None) # 0.5(ksize-1)
 
-# Choose denoising method
-while(1):
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27:
-        break
-    h_mean = cv2.getTrackbarPos('h', 'image')
-    tsize_mean = 2 * cv2.getTrackbarPos('tws', 'image') + 1
-    ssize_mean = 2 * cv2.getTrackbarPos('sws', 'image') + 1
-    ksize_median = 2 * cv2.getTrackbarPos('k', 'image') + 1
-    if cv2.getTrackbarPos('NlMean', 'image') and cv2.getTrackbarPos('Median', 'image'):
-        img_denoised_mean = cv2.fastNlMeansDenoising(img, None, h_mean, tsize_mean, ssize_mean)
-        img_denoised = cv2.medianBlur(img_denoised_mean, ksize_median)
-    elif cv2.getTrackbarPos('Median', 'image'):
-        img_denoised = cv2.medianBlur(img, ksize_median)
-    elif cv2.getTrackbarPos('NlMean', 'image'):
-        img_denoised = cv2.fastNlMeansDenoising(img, None, h_mean, tsize_mean, ssize_mean)
-    else:
-        img_denoised = img
-    cv2.imshow('image', img_denoised)
+    # trackbars for blob detection
+    cv2.createTrackbar('Blob', 'image', 0, 1, lambda x: None)
+    cv2.createTrackbar('min_thold', 'image', 10, 255, lambda x: None)
+    cv2.createTrackbar('max_thold', 'image', 200, 255, lambda x: None)
+    cv2.createTrackbar('step', 'image', 5, 50, lambda x: None)
+    cv2.createTrackbar('min_area', 'image', 5, 100, lambda x: None)
+    # cv2.imshow('image', img)
 
-cv2.destroyAllWindows()
+    # Choose denoising method
+    while(1):
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+        # denoise
+        nlmean_denoise = cv2.getTrackbarPos('NlMean', 'image')
+        median_denoise = cv2.getTrackbarPos('Median', 'image')
+        h_mean = cv2.getTrackbarPos('h', 'image')
+        tsize_mean = 2 * cv2.getTrackbarPos('tws', 'image') + 1
+        ssize_mean = 2 * cv2.getTrackbarPos('sws', 'image') + 1
+        ksize_median = 2 * cv2.getTrackbarPos('k', 'image') + 1
+        if nlmean_denoise and median_denoise:
+            img_denoised_mean = cv2.fastNlMeansDenoising(img, None, h_mean, tsize_mean, ssize_mean)
+            img_denoised = cv2.medianBlur(img_denoised_mean, ksize_median)
+        elif median_denoise:
+            img_denoised = cv2.medianBlur(img, ksize_median)
+        elif nlmean_denoise:
+            img_denoised = cv2.fastNlMeansDenoising(img, None, h_mean, tsize_mean, ssize_mean)
+        else:
+            img_denoised = img
+        # blob detection
+        if cv2.getTrackbarPos('Blob', 'image'):
+            min_thold = cv2.getTrackbarPos('min_thold', 'image')
+            max_thold = cv2.getTrackbarPos('max_thold', 'image')
+            step = cv2.getTrackbarPos('step', 'image')
+            min_area =  cv2.getTrackbarPos('min_area', 'image')
+            keypoints = particle_blob_detection(img_denoised, 
+                                                min_area=min_area, 
+                                                min_threshold=min_thold, 
+                                                max_threshold=max_thold, 
+                                                step_threshold=step)
+            img_blob = cv2.drawKeypoints(img_denoised, keypoints, np.array([]), (0,0,255), 
+                                        cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        else:
+            img_blob = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        img_denoised = cv2.cvtColor(img_denoised, cv2.COLOR_GRAY2BGR)
+        img_show = np.hstack((img_denoised, img_blob))
+        cv2.imshow('image', img_show)
+
+    cv2.destroyAllWindows()
+    return (nlmean_denoise, h_mean, tsize_mean, ssize_mean, ksize_median, 
+            median_denoise, ksize_median, min_thold, max_thold, step, min_area)
